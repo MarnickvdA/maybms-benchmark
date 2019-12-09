c2cafc905b168c46071689b9a14d4c9ddf802e11
MarnickvdA <marnick@vdarend.com>
2019-12-09 16:28:19 +0100

    Added more information about the benchmark

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ This research project will provide a benchmark for comparing these databases. Th
 	
 ## Setup
 
-This project uses PyCharm as the IDE and [https://www.anaconda.com/](Anaconda) for easy package management.
+This project uses PyCharm as the IDE and [Anaconda](https://www.anaconda.com/) for easy package management.
 
 Import this project into PyCharm and execute the following in the PyCharm terminal:
 
@@ -28,6 +28,8 @@ Import this project into PyCharm and execute the following in the PyCharm termin
 $ bash bin/setup.sh
 ```
 
-## Usage
+## Useful links
 
-...
+- [Postgres 8.3 SQL Commands](https://www.postgresql.org/docs/8.3/sql-commands.html)
+- [MayBMS Language Reference](http://maybms.sourceforge.net/manual/index.html#x1-450006.2)
+
--- maybms_benchmark/benchmark/benchmark.py
+++ maybms_benchmark/benchmark/benchmark.py
@@ -1,8 +1,28 @@
 from helpers.sql_helper import execute_query as query
 
+# Running on top of Postgres 8.3: https://www.postgresql.org/docs/8.3/sql-commands.html
+# Language reference for MayBMS: http://maybms.sourceforge.net/manual/index.html#x1-450006.2
 
 def runBenchmark(connection):
-    # Case 1
+    # Dummy case
     query(connection, "SELECT * FROM *")
+
+    # Take a database table R with the column P, to create a probabilistic database we must use the statement 'repair key'.
+    # This creates a tuple of the possible outcomes paired with the probability that the event occurs
+
+    # create table S as
+    # repair key Dummy in R weight by P;
+
+    # Now that we have a probabilistic table S, we can use the statement 'conf()' to do queries according to the confidence
+
+    # Case 1: where
+
+    # Case 2: where, group by
+
+    # Case 3: weight by
+
+    # Case 4:
+
+    # Case 5: sorting by probability
 
     return 0
