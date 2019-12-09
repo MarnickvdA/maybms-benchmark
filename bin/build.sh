a51fa6a3bd0c29790683b5c22eb70b19777ef433
MarnickvdA <marnick@vdarend.com>
2019-12-09 15:16:34 +0100

    Deleted env files

--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+
+# Commands that require you to be logged into your conda environment
+conda install -c anaconda psycopg2
--- maybms_benchmark/app.py
+++ maybms_benchmark/app.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python
 def run():
+    print("Running!")
     return 0
