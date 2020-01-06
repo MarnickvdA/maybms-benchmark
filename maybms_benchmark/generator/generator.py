2c4c5891f368b8807d7023c4f7791b40b8c02620
JelleBeerten <451490@student.saxion.nl>
2020-01-06 21:04:16 +0100

    added structure to generator

--- maybms_benchmark/generator/generator.py
+++ maybms_benchmark/generator/generator.py
@@ -6,7 +6,6 @@ from random import random
 
 from helpers.sql_helper import execute_query as query
 
-holder = []
 seed(3)
 
 # Simple routine to run a query on a database and print the results:
@@ -15,7 +14,7 @@ def doQuery(conn):
     print(query(conn, "SELECT * FROM k"))
 
 
-def write_to_output(file):
+def write_to_output(file, holder):
     with open(file, 'w') as csvoutput:
         writer = csv.writer(csvoutput, lineterminator='\n')
         writer.writerows(holder)
@@ -30,48 +29,56 @@ def show_file(file):
                 print("\n" f'Column names are >> {", ".join(row)}')
                 line_count += 1
             else:
-                print(f'\t{row[0]} , {row[1]} , {row[2]} , {row[3]}.')
+                print(f'{row[0]} , {row[1]} , {row[2]} , {row[3]}, {row[4]}, {row[5]}.')
                 line_count += 1
         print(f'Processed {line_count} lines.')
 
 
-def cvs_reader(input, output):
+def alter_dataset(input, output):
     with open(input, 'r') as csvinput:
+        holder = []
         reader = csv.reader(csvinput)
         row = next(reader)
-        row.append(' P')
+        row.append('Weight')
+        row.append('P')
         print(row)
-        holder.clear()
+        #holder.clear()
         holder.append(row)
 
         for row in reader:
             inverse, value, second_value = probability_generator()
             row_value = row.copy()
             row_inverse = row.copy()
+
+            row_value.append('1')
             row_value.append(value)
+
+            row_inverse.append('2')
             row_inverse.append(inverse)
+
             holder.append(row_value)
             holder.append(row_inverse)
+
             if second_value is not 0:
                 row_second_value = row.copy()
+                row_second_value.append('3')
                 row_second_value.append(second_value)
                 holder.append(row_second_value)
 
-        write_to_output(output)
-        # show_file
+        write_to_output(output, holder)
 
 
 def check_processed(input, output):
     with open(output, 'r') as csvin:
         read = csv.reader(csvin)
         if os.stat(output).st_size == 0:
-            cvs_reader(input, output)
+            alter_dataset(input, output)
         else:
             check = next(read)
             if 'P' in check:
                 print('File already processed!')
             else:
-                cvs_reader(input, output)
+                alter_dataset(input, output)
 
 
 def probability_generator():
@@ -91,4 +98,7 @@ def run(connection):
     print("hello")
     # Do cool generator stuff...
     # ...
-    check_processed('dataset', 'processed')
+
+
+check_processed('dataset', 'processed')
+show_file('processed')
